b22417a2ff6bb550d8ff32d73fd48cc141c5f8d1
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-02 18:26:37 +0530

    Update AfterInstallHook.sh

--- AfterInstallHook.sh
+++ AfterInstallHook.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-npm install
-npm init -y
-node app.js
 
+# Change to the directory where your Node.js application is located
+cd /opt/codedeploy-agent
+
+# Install dependencies specified in package.json
+npm install
+
+# Run the Node.js application
+node app.js
