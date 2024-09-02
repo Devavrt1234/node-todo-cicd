dcdc80514f01c6e0a6f8ba1e9197412e4c4c28a4
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-02 23:03:17 +0530

    Update BeforeInstallHook.sh

--- BeforeInstallHook.sh
+++ BeforeInstallHook.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 sudo apt update -y
+sudo npm install
+sudo install nodejs
+sudo systemctl status codedeploy-agent
+sudo systemctl restart codedeploy-agent
 
+
+
+dir_path="/home/ubuntu/views"  # replace with the directory you want to check
+
+if [ -d "$dir_path" ]; then
+    echo "The directory exists code is brought by aws codedeploy-agent successfully"
+else
+    echo "The directory does not exist.some error occurred"
+fi
