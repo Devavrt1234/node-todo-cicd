e18f0b95052393b81bab69655d640d574f335ba4
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-02 23:45:56 +0530

    Update BeforeInstallHook.sh

--- BeforeInstallHook.sh
+++ BeforeInstallHook.sh
@@ -7,11 +7,10 @@ sudo systemctl restart codedeploy-agent
 
 
 
-dir_path="/home/ubuntu/views"  # replace with the directory you want to check
+dir_path="/opt/codedeploy-agent/deployment-root/a6fd13df-97f3-49db-ae6e-7dbe2e25062c/d-6DUL4VD58/deployment-archive"  # replace with the directory you want to check
 
 if [ -d "$dir_path" ]; then
     echo "The directory exists code is brought by aws codedeploy-agent successfully"
-    cd /opt/codedeploy-agent/deployment-root/a6fd13df-97f3-49db-ae6e-7dbe2e25062c/d-5NNB8XC58/deployment-archive
     pm2 start app.js
 else
 
