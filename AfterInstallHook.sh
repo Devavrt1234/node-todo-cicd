cac7aa379880f46caed14218c58a42dea53ff3b7
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-02 22:53:07 +0530

    Update AfterInstallHook.sh

--- AfterInstallHook.sh
+++ AfterInstallHook.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Change to the directory where your Node.js application is located
-cd /home/ubuntu
-
 # Install dependencies specified in package.json
 npm install
-
 # Run the Node.js application
-node app.js
