cace897a0756fdef2af14323288d0a5b7e26099c
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-02 22:56:31 +0530

    Update AfterInstallHook.sh

--- AfterInstallHook.sh
+++ AfterInstallHook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Install dependencies specified in package.json
-npm install
+# Install dependencies specified in package.js
+echo 'I can not run anything all commands deleted'
 # Run the Node.js application
