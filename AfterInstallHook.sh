7facbe224400bf9654fdb866c5a88317fa128fc9
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-04 00:20:38 +0530

    Update AfterInstallHook.sh

--- AfterInstallHook.sh
+++ AfterInstallHook.sh
@@ -2,12 +2,7 @@
 
 # Get the list of directories in the current directory
 
-
-
-
-cd d-9NW56YU58    
-cd deployment-archive
-sudo apt install pm2@latest -g
+cd /d-IDFY3OV58/deployment-archieve
 node app.js
     
 
