1110c03ed6ff81a2915c42a21e55fd7a2c410d30
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-03 13:42:58 +0530

    Update BeforeInstallHook.sh

--- BeforeInstallHook.sh
+++ BeforeInstallHook.sh
@@ -10,11 +10,10 @@ sudo systemctl restart codedeploy-agent
 dir_path="/opt/codedeploy-agent/deployment-root/a6fd13df-97f3-49db-ae6e-7dbe2e25062c/d-6DUL4VD58/deployment-archive"  # replace with the directory you want to check
 
 if [ -d "$dir_path" ]; then
-    echo "The directory exists code is brought by aws codedeploy-agent successfully"
+    echo "The directory exists code is brought by aws code deploy-agent successfully"
     cd "$dir_path"
-    npm install pm2@latest -g
     pm2 start app.js
 else
 
-    echo "The directory does not exist.some error occurred"
+    echo "The directory does not exist. some error occurred"
 fi
