0e373baf263e9775508f8b3ab78fd0ea87df8d4c
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-03 15:22:41 +0530

    Update BeforeInstallHook.sh

--- BeforeInstallHook.sh
+++ BeforeInstallHook.sh
@@ -5,7 +5,5 @@ sudo npm install
 sudo systemctl status codedeploy-agent
 sudo systemctl restart codedeploy-agent
 
-dir_path="/opt/codedeploy-agent/deployment-root/a6fd13df-97f3-49db-ae6e-7dbe2e25062c"
-ls -la
-cd d-BSOQHZS58/deployment-archive
+dir_path="/opt/codedeploy-agent/deployment-root/a6fd13df-97f3-49db-ae6e-7dbe2e25062c/d-6YG413T58/deployment-archive"
 node app.js
