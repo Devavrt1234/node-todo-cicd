f46915b05133bb7bf29d09ac8e56bdd2415c74e3
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-04 00:04:47 +0530

    Update AfterInstallHook.sh

--- AfterInstallHook.sh
+++ AfterInstallHook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the list of directories in the current directory
-#directories="/opt/codedeploy-agent/deployment-root/a6fd13df-97f3-49db-ae6e-7dbe2e25062c"
+directories="/opt/codedeploy-agent/deployment-root/a6fd13df-97f3-49db-ae6e-7dbe2e25062c"
 
 # Get the newest directory
 #newest_dir=$(printf "%s\n" "${directories[@]}" | sort -n | tail -1)
@@ -10,18 +10,14 @@
 #rsync -avz "$newest_dir"/ /home/ubuntu/
 
 
-directories=("d-9NW56YU58" "d-IC8AAUU58" "d-IDFY3OV58" "d-OFYXVNU58" "d-X7Y347V58")
 
-for dir in "${directories[@]}"; do
-  if [ -d "$dir" ]; then
-    cd "$dir"
+  if [ -d "$directories" ]; then
+    cd d-9NW56YU58    
     cd deployment-archive
     sudo apt install pm2@latest -g
     pm2 status
     pm2 list
     pm2 start app.js
-    break
-  fi
-done
+    
 
 
