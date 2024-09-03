a1122bd2194b6dcdd12ee9ab89d46944826d62b4
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-03 11:49:15 +0530

    Update BeforeInstallHook.sh

--- BeforeInstallHook.sh
+++ BeforeInstallHook.sh
@@ -12,7 +12,7 @@ dir_path="/opt/codedeploy-agent/deployment-root/a6fd13df-97f3-49db-ae6e-7dbe2e25
 if [ -d "$dir_path" ]; then
     echo "The directory exists code is brought by aws codedeploy-agent successfully"
     cd "$dir_path"
-    node app.js
+    pm2 start app.js
 else
 
     echo "The directory does not exist.some error occurred"
