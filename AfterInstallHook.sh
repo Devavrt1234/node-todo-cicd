fdf2abce81e884200e09a27a0591c2bbc0fa350a
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-04 00:36:29 +0530

    Update AfterInstallHook.sh

--- AfterInstallHook.sh
+++ AfterInstallHook.sh
@@ -3,7 +3,8 @@
 # Get the list of directories in the current directory
 pwd
 
-cd /d-IDFY3OV58/deployment-archieve
+cd /opt/codedeploy-agent/deployment-root/a6fd13df-97f3-49db-ae6e-7dbe2e25062c/d-XLVDBJ368/deployment-archive
+
 sudo apt install pm2@latest -g
 pm2 start app.js
     
