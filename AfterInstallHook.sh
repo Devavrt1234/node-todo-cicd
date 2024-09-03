19d84d0b3a5f84dba37fa544672d75c8cc8303fe
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-04 00:10:18 +0530

    Update AfterInstallHook.sh

--- AfterInstallHook.sh
+++ AfterInstallHook.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 # Get the list of directories in the current directory
-directories="/opt/codedeploy-agent/deployment-root/a6fd13df-97f3-49db-ae6e-7dbe2e25062c"
 
-# Get the newest directory
-#newest_dir=$(printf "%s\n" "${directories[@]}" | sort -n | tail -1)
 
-# Rsync the newest directory to /home/ubuntu
-#rsync -avz "$newest_dir"/ /home/ubuntu/
 
 
 cd d-9NW56YU58    
