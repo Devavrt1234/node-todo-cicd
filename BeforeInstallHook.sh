501bd0a636df913cb2413aef2fb111feaed7b42d
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-03 15:29:58 +0530

    Update BeforeInstallHook.sh

--- BeforeInstallHook.sh
+++ BeforeInstallHook.sh
@@ -6,4 +6,9 @@ sudo systemctl status codedeploy-agent
 sudo systemctl restart codedeploy-agent
 
 dir_path="/opt/codedeploy-agent/deployment-root/a6fd13df-97f3-49db-ae6e-7dbe2e25062c/d-6YG413T58/deployment-archive"
+if [ -d "$dir_path" ]; then
+cd "$dir_path"
 node app.js
+else 
+  echo "not going anywhere"
+fi
