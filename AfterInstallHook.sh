c421a7d0166faeffa43380efaf0beebb1531d94d
Devavrt1234 <95224039+Devavrt1234@users.noreply.github.com>
2024-09-04 11:10:13 +0530

    Update AfterInstallHook.sh

--- AfterInstallHook.sh
+++ AfterInstallHook.sh
@@ -5,7 +5,7 @@ pwd
 
 rsync -avz /opt/codedeploy-agent/deployment-root/a6fd13df-97f3-49db-ae6e-7dbe2e25062c/ /home/ubuntu
 
-cd /home/ubuntu/d-OFYXVNU58/deployment-archieve 
+cd d-OFYXVNU58/deployment-archieve 
 
 sudo apt install pm2@latest -g
 pm2 -f stop  --update-env  app.js
